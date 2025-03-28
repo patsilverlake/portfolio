d4511271b3720b372df9d5c1285e8ddbd5c5cb44
patsilverlake <86451626+patsilverlake@users.noreply.github.com>
2025-03-28 21:09:30 +0400

    init edits to portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -11,15 +11,32 @@ export default function Page() {
       />
       <p className="mb-4">
         {`As a casual, 
-        I found the most recent developments in AI to be the spark that reignited my drive to build practical,
+        the recent developments in AI has been a spark that reignited my passion for building practical,
          impactful tools for personal productivity and potentially for nimble, high-performing teams.`}
       </p>
       <p className="mb-8">
-        {`That spark came during my studies at CBS, where Professor Daniel Guetta's courses gave me the analytical and statistical framework to understand machine learning models. Though my background is rooted in finance, AI has inspired me to build tools that optimize my daily workflow and brainstorm strategies with businesses—whether it's an AI-driven analytics MVP or a product idea to streamline operations. Using tools like Cursor, Vercel, Supabase, and more, I've been developing solutions that blend my investment expertise with a hands-on approach to technology, and this portfolio showcases that journey.`}
+        {`I owe a lot of gratitude to Professor Daniel Guetta during my time at CBS, whose courses provided me with the analytical and statistical framework to understand machine learning models. Though I'm just a finance guy, AI has inspired me to build tools that optimize my daily workflow and brainstorm strategies with businesses—whether it's an AI-driven analytics MVP or a product idea to streamline operations. Using tools like Cursor, Vercel, Supabase, and much more, I've been developing solutions that blend my finance experience with a hands-on approach to tool building, and I'm proud to showcase that journey.`}
       </p>
 
       <h2 className="font-semibold text-2xl mb-4 tracking-tighter">Projects</h2>
       <div className="space-y-4">
+        
+      <a 
+          href="https://animalkingdom-six.vercel.app/"
+          className="block group"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="transform transition-all">
+            <h3 className="font-medium text-neutral-900 dark:text-neutral-100 group-hover:text-neutral-800 dark:group-hover:text-neutral-200">
+              Animal Kingdom - Dodger Game
+            </h3>
+            <p className="text-neutral-600 dark:text-neutral-400">
+              Fun mobile game for kids!
+            </p>
+          </div>
+        </a>
+        
         <a 
           href="https://streetwear-gghd.vercel.app/"
           className="block group"
