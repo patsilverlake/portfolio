48e50a18da92554359e296af5197876b9d3ac93e
patsilverlake <86451626+patsilverlake@users.noreply.github.com>
2025-03-28 14:01:50 +0400

    init change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
+    default: 'Portfolio',
     template: '%s | Next.js Portfolio Starter',
   },
   description: 'This is my portfolio.',
